ecbcd68fc643bb498c93e64e21d4baacb026233a
Rafał Kulawiak <62555963+Snip33x@users.noreply.github.com>
2021-10-17 22:56:57 +0200

    Trigger Death Anim, + stoping attacking when move

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -13,7 +13,7 @@ namespace RPG.Combat
         [SerializeField] float timeBetweenAttacks = 1f;
         [SerializeField] float weaponDamage = 5f;
 
-        Transform target; //!!!! FIND OUT how this target is set up
+        Health target; //!!!! FIND OUT how this target is set up-- its dont in Attack method:)  //we changed it from transform to health to be more specific , no need to getcomponent now
         float timeSinceLastAttack = 0;
 
 
@@ -24,9 +24,11 @@ namespace RPG.Combat
 
             if (target == null) return;
 
+            if (target.IsDead()) return;
+
             if (!GetIsInRange()) //if (target != null && !GetIsInRange()) IMPORTANT NOTE  && operator, if the first thing is false it skips evaluating the second because the result will alwyas be false -- optimalization! - this way we got rid of null reference error -- eventualy we had a problem here that we were stopping character all the time, so return early //quiz Bacis Combat #1
             {
-                GetComponent<Mover>().MoveTo(target.position);
+                GetComponent<Mover>().MoveTo(target.transform.position);
             }
             else
             {
@@ -48,25 +50,25 @@ namespace RPG.Combat
         // Animation Event
         void Hit()
         {
-            Health healthComponent = target.GetComponent<Health>();
-            healthComponent.TakeDamage(weaponDamage);
+            target.TakeDamage(weaponDamage); // removing get component and turing it into this line
         }
 
         private bool GetIsInRange()
         {
-            return Vector3.Distance(transform.position, target.position) < weaponRange; //we are not using navmeshagent.remainingdistance because it is not calculating real distance, - as remainingDistance is a calculation what what actually needs to be travelled to get to the location.
+            return Vector3.Distance(transform.position, target.transform.position) < weaponRange; //we are not using navmeshagent.remainingdistance because it is not calculating real distance, - as remainingDistance is a calculation what what actually needs to be travelled to get to the location.
         }
 
         public void Attack(CombatTarget combatTarget)
         {
             GetComponent<ActionScheduler>().StartAction(this);
             //print("Take that you short, squat peasant!");
-            target = combatTarget.transform;
+            target = combatTarget.GetComponent<Health>();
         }
 
 
         public void Cancel()
         {
+            GetComponent<Animator>().SetTrigger("stopAttack"); //trigger in transition to make our character immidietly stop attacking animation when we move
             target = null;
         }
 
--- Assets/Scripts/Combat/Health.cs
+++ Assets/Scripts/Combat/Health.cs
@@ -6,15 +6,34 @@ namespace RPG.Combat
 {
     public class Health : MonoBehaviour
     {
-        [SerializeField] float health = 100f;
+        [SerializeField] float healthPoints = 100f;
+
+        bool isDead = false;
+
+        public bool IsDead()
+        {
+            return isDead;
+        }
 
         public void TakeDamage(float damage)
         {
             //health -= damage; // my example to make health not go beyond 0
             //if (health < 0)
             //    health = 0;
-            health = Mathf.Max(health - damage, 0);
-            print(health); //cos
+            healthPoints = Mathf.Max(healthPoints - damage, 0);
+            print(healthPoints); //cos
+            if(healthPoints <= 0)
+            {
+                Die();
+            }
+        }
+
+        private void Die()
+        {
+            if (isDead) return;
+
+            isDead = true;
+            GetComponent<Animator>().SetTrigger("die");
         }
     }
 }
