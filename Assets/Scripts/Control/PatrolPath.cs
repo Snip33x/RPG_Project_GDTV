6967cc409f0028011d52a70894092a6cb45c36f4
Rafał Kulawiak <62555963+Snip33x@users.noreply.github.com>
2021-11-01 23:30:51 +0100

    Waypoint Loops

--- Assets/Scripts/Control/PatrolPath.cs
+++ Assets/Scripts/Control/PatrolPath.cs
@@ -8,12 +8,29 @@ namespace RPG.Control
     {
         [SerializeField] const float waypointGizmoRadius = 0.3f;
 
+
         private void OnDrawGizmos()
         {
             for (int i = 0; i < transform.childCount; i++)
             {
-                Gizmos.DrawSphere(transform.GetChild(i).position, waypointGizmoRadius);
+                int j = GetNextIndex(i);
+                Gizmos.DrawSphere(GetWaypoint(i), waypointGizmoRadius);
+                Gizmos.DrawLine(GetWaypoint(i), GetWaypoint(j));
             }
+        }
+
+        private int GetNextIndex(int i)  //static here was preventing me from using transform.childCount - check
+        {
+            if (i + 1 == transform.childCount)
+            {
+                return 0;
+            }
+            return i + 1;
+        }
+
+        private Vector3 GetWaypoint(int i) //extraced method
+        {
+            return transform.GetChild(i).position;
         }
     }
 }
