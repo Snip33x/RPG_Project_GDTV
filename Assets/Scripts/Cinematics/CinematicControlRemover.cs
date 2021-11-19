d47d06cf51fe4890405363db5afe134e751de1ec
Rafał Kulawiak <62555963+Snip33x@users.noreply.github.com>
2021-11-19 23:30:58 +0100

    Player Input in Cutscenes

--- Assets/Scripts/Cinematics/CinematicControlRemover.cs
+++ Assets/Scripts/Cinematics/CinematicControlRemover.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
+using RPG.Core;
+using RPG.Control;
 
 //OBSERVER PATTERN
 
@@ -9,20 +11,24 @@ namespace RPG.Cinematics
 {
     public class CinematicControlRemover : MonoBehaviour
     {
+        GameObject player;
+
         private void Start()
         {
             GetComponent<PlayableDirector>().played += DisableControl; //played is event  ctrl+mouse on played
             GetComponent<PlayableDirector>().stopped += EnableControl; //stopped is event
+            player = GameObject.FindWithTag("Player");
         }
 
         void DisableControl(PlayableDirector pd) //argument must be putted here becasue in PlayabeDirectrol class -- public event Action<PlayableDirector> played;  -- in Action<> there is PlayableDirector
-        {
-            print("DisableControl");
+        {       
+            player.GetComponent<ActionScheduler>().CancelCurrentAction();
+            player.GetComponent<PlayerController>().enabled = false;
         }
 
         void EnableControl(PlayableDirector pd)
         {
-            print("EndbleControl");
+            player.GetComponent<PlayerController>().enabled = true;
         }
     }
 }
