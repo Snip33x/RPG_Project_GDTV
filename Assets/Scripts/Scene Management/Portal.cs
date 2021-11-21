808cdf5ff53664737dce66bf2ca93716421d0087
Rafał Kulawiak <62555963+Snip33x@users.noreply.github.com>
2021-11-22 00:13:05 +0100

    Player Spawn Point

--- Assets/Scripts/Scene Management/Portal.cs
+++ Assets/Scripts/Scene Management/Portal.cs
@@ -1,13 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.AI;
 
 namespace RPG.SceneManagement
 {
     public class Portal : MonoBehaviour
     {
         [SerializeField] int sceneToLoad = -1; // we will remember to change it in the inspector to right one
+        [SerializeField] Transform spawnPoint;
+
+        GameObject player;
+
 
         private void OnTriggerEnter(Collider other)
         {
@@ -21,8 +27,34 @@ namespace RPG.SceneManagement
         {
             DontDestroyOnLoad(gameObject);
             yield return SceneManager.LoadSceneAsync(sceneToLoad);  //Unity knows that it needs to run this coroutine once the scene is loaded
-            print("Scene Loaded");
+
+            Portal otherPortal = GetOtherPortal();
+            UpdatePlayer(otherPortal);
+
             Destroy(gameObject);
         }
+
+        private Portal GetOtherPortal()
+        {
+            foreach (Portal portal in FindObjectsOfType<Portal>())
+            {
+                if (portal == this) continue;
+
+                return portal;
+            }
+
+            return null;
+        }
+
+        private void UpdatePlayer(Portal otherPortal)
+        {
+            player = GameObject.FindWithTag("Player");
+            //if our character is spawning at wrong locations reenable navMeshAgent
+            player.GetComponent<NavMeshAgent>().enabled = false;  // or use .Warp
+            player.transform.position = otherPortal.spawnPoint.position;
+            player.transform.rotation = otherPortal.spawnPoint.rotation;
+            player.GetComponent<NavMeshAgent>().enabled = true;
+        }
+
     }
 }
