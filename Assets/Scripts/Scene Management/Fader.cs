e268f4dd0a9a79c5070aab08edd3d7f69823c8c0
Rafał Kulawiak <62555963+Snip33x@users.noreply.github.com>
2021-11-23 21:18:53 +0100

    Nested Coroutines For Fading

---
+++ Assets/Scripts/Scene Management/Fader.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.SceneManagement
+{
+    public class Fader : MonoBehaviour
+    {
+        CanvasGroup canvasGroup;
+
+        private void Start()
+        {
+            canvasGroup = GetComponent<CanvasGroup>();
+
+            StartCoroutine(FadeOutIn());
+        }
+
+        IEnumerator FadeOutIn() //nested Coroutine
+        {
+            yield return FadeOut(3f);  //every statement is executed after the previous has ended
+            print("Faded out");
+            yield return FadeIn(2f);
+            print("Faded in");
+        }
+
+        public IEnumerator FadeOut(float time)
+        {
+            while (canvasGroup.alpha < 1) // alpha is not 1
+            {
+                canvasGroup.alpha += Time.deltaTime / time;
+                // moving alpha toward 1
+                yield return null; //run on the next frame
+            }
+        }
+
+        public IEnumerator FadeIn(float time)
+        {
+            while (canvasGroup.alpha > 0) // alpha is not 1
+            {
+                canvasGroup.alpha -= Time.deltaTime / time;
+                // moving alpha toward 1
+                yield return null; //run on the next frame
+            }
+        }
+    }
+
+}
+
