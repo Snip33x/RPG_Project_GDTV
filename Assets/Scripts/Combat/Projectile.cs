f92ebc1837742ce4d3aa3730e1212216f1af95f9
Rafał Kulawiak <62555963+Snip33x@users.noreply.github.com>
2021-12-13 21:18:36 +0100

    Improve Projectile Behaviour

--- Assets/Scripts/Combat/Projectile.cs
+++ Assets/Scripts/Combat/Projectile.cs
@@ -9,16 +9,28 @@ namespace RPG.Combat
     public class Projectile : MonoBehaviour
     {
         [SerializeField] float projectileSped = 1f;
+        [SerializeField] bool isHoming = true; // chasing target
 
         Health target = null;
         float damage = 0;
+
+
+        private void Start()  
+        {
+            transform.LookAt(GetAimLocation());  //everytime projectile is Instantiated we set target location
+        }
 
         void Update()
         {
             if (target == null) return;
 
-            transform.LookAt(GetAimLocation());
+
+            if (isHoming && !target.IsDead()) //avoid arrow sticking in air at target location when target is dead
+            {
+                transform.LookAt(GetAimLocation());
+            }
             transform.Translate(Vector3.forward * Time.deltaTime * projectileSped);
+
         }
 
         public void SetTarget(Health target, float damage) //we are calculating damage done by projectile at the shoot moment
@@ -40,6 +52,7 @@ namespace RPG.Combat
         private void OnTriggerEnter(Collider other)
         {
             if (other.GetComponent<Health>() != target) return;
+            if (target.IsDead()) return; //if enemy is dead don't try to give any damage and don't destroy object
             target.TakeDamage(damage);
             Destroy(gameObject);
 
