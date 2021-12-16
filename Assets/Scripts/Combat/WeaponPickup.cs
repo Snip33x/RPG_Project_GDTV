817b4b1a11a991d1acee5d7e02c91bb0028809ea
Rafał Kulawiak <62555963+Snip33x@users.noreply.github.com>
2021-12-16 13:53:40 +0100

    Pickup Respawning -- disable childs in Parent

--- Assets/Scripts/Combat/WeaponPickup.cs
+++ Assets/Scripts/Combat/WeaponPickup.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,15 +8,35 @@ namespace RPG.Combat
     public class WeaponPickup : MonoBehaviour
     {
         [SerializeField] Weapon weapon = null;
+        [SerializeField] float timeToRespawnAnItem = 5f;
 
         private void OnTriggerEnter(Collider other)
         {
             if (other.gameObject.tag == "Player")
             {
                 other.GetComponent<Fighter>().EquipWeapon(weapon);
-                Destroy(gameObject);
+                StartCoroutine(HideForSeconds(timeToRespawnAnItem)); //we are not disabling whole gameObject because the Coroutine would fail to run
             }
         }
+
+        private IEnumerator HideForSeconds(float seconds) 
+        {
+            ShowPickup(false);
+            yield return new WaitForSeconds(seconds);
+            ShowPickup(true);
+        }
+
+        //!!! Disabling and reenabling childs in Parent 
+        private void ShowPickup(bool shouldShow)
+        {
+            GetComponent<Collider>().enabled = shouldShow;
+            foreach (Transform child in transform) //transform is refering to this object transform
+            {
+                child.gameObject.SetActive(shouldShow);
+            }
+        }
+
+
     }
 }
 
