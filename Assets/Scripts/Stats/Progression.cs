86fc427c7a8c1b88fb97f9ac20fd8cba637ce1c3
Rafał Kulawiak <62555963+Snip33x@users.noreply.github.com>
2021-12-18 23:29:20 +0100

    Editing Scriptable Object FIles - YAML

--- Assets/Scripts/Stats/Progression.cs
+++ Assets/Scripts/Stats/Progression.cs
@@ -13,7 +13,7 @@ namespace RPG.Stats
             {
                 if (progressionClass.characterClass == characterClass)
                 {
-                    return progressionClass.health[level - 1];
+                    //return progressionClass.health[level - 1];
                 }
             }
             return 0;
@@ -23,7 +23,15 @@ namespace RPG.Stats
         class ProgressionCharacterClass
         {
             public CharacterClass characterClass;
-            public float[] health;
+            public ProgressionStat[] stats;
+            
+        }
+
+        [System.Serializable]
+        class ProgressionStat
+        {
+            public Stat stat;
+            public float[] levels;
         }
     }
 
