a41f5878407c2cf1cefe2ef15d3e481505659234
Rafał Kulawiak <62555963+Snip33x@users.noreply.github.com>
2021-12-21 15:01:21 +0100

    Events and Delegates

--- Assets/Scripts/Stats/BaseStats.cs
+++ Assets/Scripts/Stats/BaseStats.cs
@@ -11,13 +11,22 @@ namespace RPG.Stats
         [SerializeField] CharacterClass characterClass;
         [SerializeField] Progression progression = null;
 
+        int currentLevel = 0;
+
+        private void Start()
+        {
+            currentLevel = CalculateLevel();    
+        }
 
         private void Update()
         {
-            if(gameObject.tag == "Player")
+            int newLevel = CalculateLevel();
+            if (newLevel > currentLevel)
             {
-                print(GetLevel());
+                currentLevel = newLevel;
+                print("Leveled Up!");
             }
+
         }
 
         public float GetStat(Stat stat)
@@ -26,6 +35,11 @@ namespace RPG.Stats
         }
 
         public int GetLevel()
+        {
+            return currentLevel;
+        }
+
+        public int CalculateLevel()
         {
             Experience experience = GetComponent<Experience>();
 
