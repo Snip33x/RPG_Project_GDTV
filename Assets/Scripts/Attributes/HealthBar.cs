0d7c06ca742c81994bc958d1eb30708c4a21e092
Rafał Kulawiak <62555963+Snip33x@users.noreply.github.com>
2022-01-17 23:32:16 +0100

    Disabling Empty Health Bar

--- Assets/Scripts/Attributes/HealthBar.cs
+++ Assets/Scripts/Attributes/HealthBar.cs
@@ -8,10 +8,20 @@ namespace RPG.Attributes
     {
         [SerializeField] Health healthComponent = null;
         [SerializeField] RectTransform foreground = null;
+        [SerializeField] Canvas rootCanvas = null;
 
         void Update()
         {
+            if (Mathf.Approximately(healthComponent.GetPercentage() ,0) //float is imprecise , don't compare float to 0, use this method
+            || Mathf.Approximately(healthComponent.GetPercentage(), 100)) 
+            {
+                rootCanvas.enabled = false;
+                return;
+            }
+
+            rootCanvas.enabled = true;
             foreground.localScale = new Vector3(healthComponent.GetPercentage() / 100, 1f, 1f); //we can create a method in Health script GetFraction to avoid dividing by 100
+
         }
     }
 
